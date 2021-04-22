3e6285dfe0431fbdaab0c5fdc86cf53c3eed2a8d
Aniket <garganiket58@gmail.com>
2021-04-22 20:11:46 +0530

    initial commit

---
+++ Demo.py
@@ -0,0 +1,16 @@
+print(2+3-1)
+print(2*(10/2))
+print(0.42)
+print(4+1.65)
+print(2**3)
+print(9**(1/2))
+print(20//6)
+print(20%6)
+print("Today\'s awesome day")
+print("beautiful"+"area")
+print("this "
+      "is "
+      "me")
+print("""this 
+is 
+me""")
